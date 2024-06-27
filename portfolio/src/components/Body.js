b5eb3bf0203fd03ac6b6eafa807439611a2c7187
Eugene <abugaeugene@gmail.com>
2024-06-27 17:32:13 +0300

    added the body

--- portfolio/src/components/Body.js
+++ portfolio/src/components/Body.js
@@ -1,1 +1,28 @@
-// Create your Body component here
+import{FaGithub, FaLinkedin} from 'react-icons/fa';
+import Avatar from '../assets/image.png';
+
+const Body = () => {
+    return (
+        <div id="body" className='body'>
+            <div className="body-container">
+                <div className="body-profile">
+                    <img className="body-img" src={Avatar} alt="Avatar" />
+
+                    <div className='body-content'>
+                        <div className="body-headline">Eugene Mogeni</div>
+                        <div className="body-text">Software Engineer</div>
+                    </div>
+
+                    <div className="body-icons">
+                        <a href="https://github.com/abugaeugene"                       target="_blank" rel='noreferrer'  className="icon-link"><i><FaGithub/></i></a>
+                        <a href="https://www.linkedin.com/in/eugene-mogeni-27199a1a6/" target="_blank" rel='noreferrer'   className="icon-link"><i><FaLinkedin/></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Body;
+
+            
